3b62adefbea0746c41ee8061402394c5566913bb
Ashwani Rajput <ashwanir461@gmail.com>
2022-08-11 13:35:03 +0530

    complete

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -1,3 +1,25 @@
 public class FurnitureItem {
+    int furnitureCode;
+    String furnitureType;
+    char furnitureGrade;
+    String color;
+    String furnitureUsage;
+    double price;
 
+    FurnitureItem() {
+        furnitureCode = 210;
+        furnitureType = "Chairs";
+        furnitureGrade = 'A';
+        color = "Red";
+        furnitureUsage = "Outdoor";
+        price = 500;
+    }
+
+    double calculateDiscount(double discount) {
+        double discountPaidAmount = 0.0;
+        if (furnitureUsage.equals("Outdoor")) {
+            discountPaidAmount = price - (price * discount) / 100;
+        }
+        return discountPaidAmount;
+    }
 }
--- src/FurnitureItemImpl.java
+++ src/FurnitureItemImpl.java
@@ -1,3 +1,18 @@
 public class FurnitureItemImpl {
+    public static void main(String[] args) {
 
+        //here is the declaration
+        FurnitureItem obj;
+        //here is the initialization
+        obj = new FurnitureItem();
+
+        System.out.println("obj.furnitureType = " + obj.furnitureType);
+        System.out.println("obj.furnitureCode = " + obj.furnitureCode);
+        System.out.println("obj.furnitureGrade = " + obj.furnitureGrade);
+        System.out.println("obj.furnitureUsage = " + obj.furnitureUsage);
+        System.out.println("obj.color = " + obj.color);
+        System.out.println("Main price = " + obj.price);
+        double afterDiscountbill = obj.calculateDiscount(5);
+        System.out.println("After Discount bill = " + afterDiscountbill);
+    }
 }
